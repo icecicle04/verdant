f70b3e01288c45dbc7a5e2031ac8531ed32be216
Cody Bonsma <codybonsma@gmail.com>
2020-11-06 19:13:12 -0500

    added the plant module

---
+++ models/plant.js
@@ -0,0 +1,23 @@
+// this will hold the user plant collection
+// trefle.io for plant info/pictures etc?
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+// article schema for the mongoDB
+const plantSchema = new Schema({
+  plant_name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  plant_image: {
+    type: String,
+    data: Buffer,
+  },
+});
+
+const Plant = mongoose.model("Plant", plantSchema);
+
+module.exports = Plant;
