0d69925faf5d31dbd285b3c3ad3a0e471b33bd58
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-11-09 12:32:27 -0500

    removed signinbtn folder as its replaced by loginbtn folder

--- client/src/components/LogInBtn/index.js
+++ client/src/components/LogInBtn/index.js
@@ -4,7 +4,7 @@ import React from "react";
 function logIn() {
     return (   
       <button className="btn btn-danger">
-              <a className="login-btn" href="/logIn"> log In</a>
+              <a className="login-btn" href="/logIn">Log In</a>
       </button>
     );
   }
