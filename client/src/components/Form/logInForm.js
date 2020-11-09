e226f7977233bf9882530b2615afd02168158142
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-11-09 12:40:05 -0500

    created loginform

---
+++ client/src/components/Form/logInForm.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+
+class LogInForm extends Component {
+  // Setting the component's initial state
+  state = {
+    email: "",
+    password: ""
+  };
+
+  handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    let value = event.target.value;
+    const name = event.target.name;
+
+    if (name === "email") {
+      value = value.substring(0, 15);
+    }
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
+    if (!this.state.firstName || !this.state.lastName) {
+      alert("Please enter your email and password!");
+    }
+
+    this.setState({
+      email: "",
+      password: ""
+    });
+  };
+
+  render() {
+    // Notice how each input has a `value`, `name`, and `onChange` prop
+    return (
+      <div>
+        <form className="form">
+          <input
+            value={this.state.email}
+            name="email"
+            onChange={this.handleInputChange}
+            type="email"
+            placeholder="Email"
+          />
+            <input
+            value={this.state.password}
+            name="password"
+            onChange={this.handleInputChange}
+            type="password"
+            placeholder="Password"
+          />
+          <button onClick={this.handleFormSubmit}>Log In</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LogInForm;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import LogInForm from "../components/Form/loginform.js";
+import LogInForm from "../components/Form/LogInForm.js";
 
 function Login(props) {
   return (
