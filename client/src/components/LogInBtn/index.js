c2dee4f8eeb39a7a4abe130b120d744450e72683
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-11-09 12:30:51 -0500

    created login.js page

---
+++ client/src/components/LogInBtn/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+//Create an onClick event listener for the log-in btn
+function logIn() {
+    return (   
+      <button className="btn btn-danger">
+              <a className="login-btn" href="/logIn"> log In</a>
+      </button>
+    );
+  }
+   
+  export default logIn;
