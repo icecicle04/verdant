7d1643e96a8a3f9797964020a8d8f77776248c7a
AK <icecicle04@gmail.com>
2020-11-10 10:14:31 -0500

    git pulled from latest merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,30 @@
 import "./App.css";
 import { useEffect } from "react";
 import axios from "axios";
-import Landing from "./pages/Landing";
+import Landing from "./pages/landing";
 import Register from "./pages/Register";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Nav from "../src/components/Nav"
+import Nav from "../src/components/Nav";
 
 function App() {
   useEffect(() => {
     axios.get("/api/config").then((response) => console.log(response.data));
   }, []);
-<<<<<<< HEAD
-  return <div className="App">Hello World</div>;
-=======
   return (
     <Router>
       <div className="App">
-      <Nav />
+        <Nav />
         <Switch>
           <Route exact path={["/", "/Verdant"]}>
             <Landing />
           </Route>
-          <Route exact path={["/Register"]}>
+          <Route exact path={["/signUp"]}>
             <Register />
           </Route>
         </Switch>
       </div>
     </Router>
   );
->>>>>>> c9a5ca56ed702e7fb5b8cdcfbd6a93f74df19213
 }
 
 export default App;
