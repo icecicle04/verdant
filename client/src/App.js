2f0e0f7e97352bed467d856905b270141c0019b7
Cody Bonsma <codybonsma@gmail.com>
2020-11-11 15:33:10 -0500

    merging with branch

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,14 @@ import Account from "./pages/userAccount";
 import Articles from "./pages/Articles";
 import Plant from "./pages/Plant";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Nav from "../src/components/Nav"
+import Nav from "../src/components/Nav";
 import Login from "./pages/Login";
+import ReactDOM from "react-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+// import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faCheckSquare, faCoffee);
 
 function App() {
   useEffect(() => {
@@ -22,18 +28,18 @@ function App() {
           <Route exact path={["/", "/Verdant"]}>
             <Landing />
           </Route>
-          <Route exact path={["/Register"]}>
+          <Route exact path={["/signUp"]}>
             <Register />
           </Route>
           <Route exact path={["/Login"]}>
-            <Login/>
+            <Login />
           </Route>
           <Route exact path={["/account"]}>
             <Account />
           </Route>
           <Route exact path={["/plant"]}>
             <Plant />
-            </Route>
+          </Route>
           <Route exact path={["/Articles"]}>
             <Articles />
           </Route>
