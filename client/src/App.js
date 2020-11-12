679365f2aebc68b6eba5a1cfef38b181e8d9cb0b
Cody Bonsma <codybonsma@gmail.com>
2020-11-12 12:10:38 -0500

    used local storage for token and user information

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import AuthContext from "./context/AuthContext";
 import Landing from "./pages/Landing/Landing";
 import Register from "./pages/Register";
 import Account from "./pages/UserAccount/userAccount";
@@ -9,7 +10,8 @@ import Plant from "./pages/Plant/Plant";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "../src/components/Nav"
 import Login from "./pages/Login/Login";
-import ReactDOM from "react-dom";
+import { setAxiosDefaults } from "./utils/axiosDefaults";
+// import ReactDOM from "react-dom";
 import { library } from "@fortawesome/fontawesome-svg-core";
 // import { fab } from "@fortawesome/free-brands-svg-icons";
 import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
@@ -17,12 +19,29 @@ import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
 library.add(faCheckSquare, faCoffee);
 
 function App() {
+  const [jwt, setJwt] = useState("");
+
   useEffect(() => {
     axios.get("/api/config").then((response) => console.log(response.data));
   }, []);
+
+  useEffect(() => {
+    const localJwt = localStorage.getItem("jwt");
+    if (localJwt) {
+      setJwt(localJwt);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (jwt) {
+      setAxiosDefaults(jwt);
+      localStorage.setItem("jwt", jwt);
+    }
+  }, [jwt]);
   return (
     <Router>
       <div className="App">
+      <AuthContext.Provider value={{ jwt, setJwt }}>
         <Nav />
         <Switch>
           <Route exact path={["/", "/Verdant"]}>
@@ -44,6 +63,7 @@ function App() {
             <Articles />
           </Route>
         </Switch>
+        </AuthContext.Provider>
       </div>
     </Router>
   );
