d076e7b3b52c94349f4aafc8f91eb343809a262c
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-11-13 18:30:43 -0500

    adjusted landing page card, removed illustration

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,41 +1,53 @@
 import React from "react";
 import "./Landing.css";
-import Gardening from "../../Images/gardening.png";
+// import Gardening from "../../Images/gardening.png";
 
 function Landing(props) {
   return (
-    <div className="landingBackground">
-      <div className="container">
-        <div className="card mb-3" id="landingCard" style={{ width: "70%"}}>
-          <div className="row no-gutters">
-            <div
-              className="col-md-4"
-              id="landingPage"
-              style={{height: "10%" }}
-            >
-              <img src={Gardening} alt="Gardening" height="300" width="300" />
-            </div>
-            <div className="col-md-8">
-              <div className="card-body">
-                <h1 classNameName="landingHeading">
-                  Verdant
-                </h1>
-                <p className="card-text text-right" style={{width: "70%", justifyContent:"right"}}>
-                  Adipisicing non pariatur tempor duis sint excepteur velit excepteur eiusmod voluptate voluptate ea. Veniam est ea est magna ipsum ad ad in aliquip. Eiusmod in voluptate deserunt incididunt nisi laboris proident. Irure tempor in officia aute veniam deserunt dolor fugiat sint tempor irure nisi adipisicing
-                </p>
-              </div>
-            </div>
+      <div className="landingBackground">
+        <div className="row align-items-center justify-content-center text-center">
+          <div className="col-lg-10 align-self-end">
+            <h1 className="landingHeading text-center">Verdant</h1>
+          </div>
+          <div className="col-lg-8 align-self-baseline card-body">
+            <p className="card card-text text-center mb-5">
+              Adipisicing non pariatur tempor duis sint excepteur velit
+              excepteur eiusmod voluptate voluptate ea. Veniam est ea est magna
+              ipsum ad ad in aliquip. Eiusmod in voluptate deserunt incididunt
+              nisi laboris proident. Irure tempor in officia aute veniam
+              deserunt dolor fugiat sint tempor irure nisi adipisicing
+            </p>
           </div>
         </div>
       </div>
-    </div>
-    // <div className="container-fluid">
-    //   <div className="row">
-    //     <div className="col-md-12 text-center">
-    //     </div>
-    //   </div>
-    // </div>
   );
 }
 
 export default Landing;
+
+// <div className="landingBackground">
+//   <div className="container">
+//     <div className="card mb-3" id="landingCard" style={{ width: "70%"}}>
+//       <div className="row no-gutters">
+//         <div
+//           className="col-lg-12"
+//           id="landingPage"
+//         >
+//           <div className="card-body">
+//             <h1 className="landingHeading text-center">
+//               Verdant
+//             </h1>
+//             <p className="card-text text-center" style={{width: "70%"}}>
+//               Adipisicing non pariatur tempor duis sint excepteur velit excepteur eiusmod voluptate voluptate ea. Veniam est ea est magna ipsum ad ad in aliquip. Eiusmod in voluptate deserunt incididunt nisi laboris proident. Irure tempor in officia aute veniam deserunt dolor fugiat sint tempor irure nisi adipisicing
+//             </p>
+//           </div>
+{
+  /* <img src={Gardening} alt="Gardening" height="300" width="300" />
+            </div>
+            <div className="col-md-8" id="textColumn"> */
+}
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// </div>
