d10495f79d7c6bf72987378546b5685d32855841
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-11-13 18:50:42 -0500

    added and styled 'explore' btn on landing page. needs a route

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -9,14 +9,17 @@ function Landing(props) {
           <div className="col-lg-10 align-self-end">
             <h1 className="landingHeading text-center">Verdant</h1>
           </div>
-          <div className="col-lg-8 align-self-baseline card-body">
-            <p className="card card-text text-center mb-5">
+          <div className="col-lg-8 align-items-center justify-content-center card-body">
+            <p className="card card-text align-items-center justify-content-center text-center mb-5" style={{width: "55%"}}>
               Adipisicing non pariatur tempor duis sint excepteur velit
               excepteur eiusmod voluptate voluptate ea. Veniam est ea est magna
               ipsum ad ad in aliquip. Eiusmod in voluptate deserunt incididunt
               nisi laboris proident. Irure tempor in officia aute veniam
               deserunt dolor fugiat sint tempor irure nisi adipisicing
             </p>
+            <button id="landingBtn">
+            Explore
+          </button>
           </div>
         </div>
       </div>
