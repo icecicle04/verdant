28e6e3a7825e8f42c51fbed2f7233a1759a0384d
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-11-14 00:27:58 -0500

    got rid of freaky double-up on landing.js

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -4,75 +4,26 @@ import "./Landing.css";
 
 function Landing(props) {
   return (
-      <div className="landingBackground">
-        <div className="row align-items-center justify-content-center text-center">
-          <div className="col-lg-10 align-self-end">
-            <h1 className="landingHeading text-center">Verdant</h1>
-          </div>
-          <div className="col-lg-8 align-items-center justify-content-center card-body">
-            <p className="card card-text align-items-center justify-content-center text-center mb-5" style={{width: "55%"}}>
-              Adipisicing non pariatur tempor duis sint excepteur velit
-              excepteur eiusmod voluptate voluptate ea. Veniam est ea est magna
-              ipsum ad ad in aliquip. Eiusmod in voluptate deserunt incididunt
-              nisi laboris proident. Irure tempor in officia aute veniam
-              deserunt dolor fugiat sint tempor irure nisi adipisicing
-            </p>
-            <button id="landingBtn">
-            Explore
-          </button>
-=======
     <div className="landingBackground">
-      <div className="container">
-        <div className="card mb-3" id="landingCard" style={{ width: "50%"}}>
-          <div className="row no-gutters">
-            <div
-              className="col-md-4"
-              id="landingPage"
-              style={{height: "10%" }}
-            >
-              <img src={Gardening} alt="Gardening" height="300" width="300" />
-            </div>
-            <div className="col-md-8">
-              <div className="card-body">
-                <h1 className="landingHeading">
-                  Verdant
-                </h1>
-                <p className="card-text text-right" style={{width: "70%", justifyContent:"right"}}>
-                  Adipisicing non pariatur tempor duis sint excepteur velit excepteur eiusmod voluptate voluptate ea. Veniam est ea est magna ipsum ad ad in aliquip. Eiusmod in voluptate deserunt incididunt nisi laboris proident. Irure tempor in officia aute veniam deserunt dolor fugiat sint tempor irure nisi adipisicing
-                </p>
-              </div>
-            </div>
-          </div>
+      <div className="row align-items-center justify-content-center text-center">
+        <div className="col-lg-10 align-self-end">
+          <h1 className="landingHeading text-center">Verdant</h1>
+        </div>
+        <div className="col-lg-8 align-items-center justify-content-center card-body">
+          <p
+            className="card card-text align-items-center justify-content-center text-center mb-5"
+            style={{ width: "55%" }}
+          >
+            Adipisicing non pariatur tempor duis sint excepteur velit excepteur
+            eiusmod voluptate voluptate ea. Veniam est ea est magna ipsum ad ad
+            in aliquip. Eiusmod in voluptate deserunt incididunt nisi laboris
+            proident. Irure tempor in officia aute veniam deserunt dolor fugiat
+            sint tempor irure nisi adipisicing
+          </p>
+          <button id="landingBtn">Explore</button>
         </div>
       </div>
+    </div>
   );
 }
-
 export default Landing;
-
-// <div className="landingBackground">
-//   <div className="container">
-//     <div className="card mb-3" id="landingCard" style={{ width: "70%"}}>
-//       <div className="row no-gutters">
-//         <div
-//           className="col-lg-12"
-//           id="landingPage"
-//         >
-//           <div className="card-body">
-//             <h1 className="landingHeading text-center">
-//               Verdant
-//             </h1>
-//             <p className="card-text text-center" style={{width: "70%"}}>
-//               Adipisicing non pariatur tempor duis sint excepteur velit excepteur eiusmod voluptate voluptate ea. Veniam est ea est magna ipsum ad ad in aliquip. Eiusmod in voluptate deserunt incididunt nisi laboris proident. Irure tempor in officia aute veniam deserunt dolor fugiat sint tempor irure nisi adipisicing
-//             </p>
-//           </div>
-{
-  /* <img src={Gardening} alt="Gardening" height="300" width="300" />
-            </div>
-            <div className="col-md-8" id="textColumn"> */
-}
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-// </div>
