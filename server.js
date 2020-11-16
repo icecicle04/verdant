2fb252d83cfb8dea1dbe265c23bfd261cf371fc6
AK <icecicle04@gmail.com>
2020-11-15 19:09:29 -0500

    hardcoded chat page and updated enviornment image

--- server.js
+++ server.js
@@ -1,30 +1,24 @@
 require("dotenv").config();
 const express = require("express");
-const http = require("http");
-const io = require("socket.io")(http);
+// const http = require("http");
+// const io = require("socket.io")(http);
 const app = express();
 const mongoose = require("mongoose");
 // consolidate this in the routes folder so we can import multiple files
 // and not just api/user
 const routes = require("./routes/api/user");
 const path = require("path");
-// const http = require("http");
-
-// const socket = require("socket.io");
 
 const router = require("./routes/api");
 
 const PORT = process.env.PORT || 3001;
 
-io.on("connection", (socket) => {
-  console.log("socket on!");
-  socket.on("message", ({ name, message }) => {
-    io.emit("message", { name, message });
-  });
-});
-// const server = http.createServer(app);
-
-// const io = socket.io(server);
+// io.on("connection", (socket) => {
+//   console.log("socket on!");
+//   socket.on("message", ({ name, message }) => {
+//     io.emit("message", { name, message });
+//   });
+// });
 
 // Add routes, both API and view
 app.use(routes);
@@ -69,6 +63,3 @@ app.get("*", (req, res) => {
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`);
 });
-
-// socket setup
-// const io = socket(server)
